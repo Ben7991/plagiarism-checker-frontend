6f6ce8adee246bb11d3a0bf6ba479b5f3030ed97
Bernard Teye <datssosh@gmail.com>
2024-08-04 21:50:45 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AuthLayout from "./components/templates/AuthLayout/AuthLayout";
+import DashboardLayout from "./components/templates/DashboardLayout/DashboardLayout";
+
+// auth pages
+import Login from "./pages/auth/Login/Login";
+import Register from "./pages/auth/Register/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword/ForgotPassword";
+import ResetPassword from "./pages/auth/ResetPassword/ResetPassword";
+
+// dashboard pages
+import Overview from "./pages/dashboard/Overview/Overview";
+import Archive from "./pages/dashboard/Archive/Archive";
+import AcademicDivision from "./pages/dashboard/AcademicDivision/AcademicDivision";
+import AccountSettings from "./pages/dashboard/AccountSettings/AccountSettings";
+import ManageUsers from "./pages/dashboard/ManageUsers/ManageUsers";
+
+export default function App() {
+  const appRouter = createBrowserRouter([
+    { 
+      path: "/", 
+      element: <AuthLayout />,
+      children: [
+        { index: true, element: <Login /> },
+        { path: "register", element: <Register /> },
+        { path: "forgot-password", element: <ForgotPassword /> },
+        { path: "reset-password", element: <ResetPassword /> },
+      ] 
+    },
+    {
+      path: "dashboard",
+      element: <DashboardLayout />,
+      children: [
+        { index: true, element: <Overview /> },
+        { path: "archive", element: <Archive /> },
+        { path: "academic-division", element: <AcademicDivision /> },
+        { path: "account-settings", element: <AccountSettings /> },
+        { path: "manage-users", element: <ManageUsers /> },
+      ]
+    }
+  ]);
+
+  return <RouterProvider router={appRouter}/>
+}
---
+++ src/pages/dashboard/AccountSettings/AccountSettings.jsx
@@ -0,0 +1,5 @@
+export default function AccountSettings() {
+  return (
+    <h1>AccountSettings</h1>
+  );
+}
