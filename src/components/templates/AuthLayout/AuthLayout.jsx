c10d3c35d84c166b73696e82511561cffeefd961
Bernard Teye <datssosh@gmail.com>
2024-08-06 05:59:45 +0000

    moved Alert, PageDescriptor, AppLogo into molecules and refactor AuthLayout to reflect changes

--- src/components/templates/AuthLayout/AuthLayout.jsx
+++ src/components/templates/AuthLayout/AuthLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 
 import styles from "./AuthLayout.module.css";
+import AppLogo from "../../molecules/AppLogo/AppLogo";
 
 export default function AuthLayout() {
   return (
-    <main>
-      <Outlet />
+    <main className={styles.main}>
+      <section className={styles.mainContent}>
+        <div className="mb-1">
+          <AppLogo />
+        </div>
+        <Outlet />
+      </section>
     </main>
   );
 }
