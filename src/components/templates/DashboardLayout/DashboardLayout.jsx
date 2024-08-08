f52a87f653bb95035230906c2f7cb88bb3cda540
Bernard Teye <datssosh@gmail.com>
2024-08-08 17:37:50 +0000

    add functionality for successfully login, error handling and redirection to the /dashboard route

--- src/components/templates/DashboardLayout/DashboardLayout.jsx
+++ src/components/templates/DashboardLayout/DashboardLayout.jsx
@@ -1,4 +1,12 @@
+import { useContext } from "react";
+
+// context
+import { StoreContext } from "../../../store/context/store.context";
+
 export default function DashboardLayout() {
+  const { auth } = useContext(StoreContext);
+  console.log(auth.user);
+
   return (
     <h1>DashboardLayout</h1>
   );
--- src/pages/auth/Login/Login.jsx
+++ src/pages/auth/Login/Login.jsx
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import { Link } from "react-router-dom";
 
+// components
 import FormGroup from "../../../components/atoms/FormGroup/FormGroup";
 import EmailIcon from "../../../components/atoms/Icons/EmailIcon";
 import LockIcon from "../../../components/atoms/Icons/LockIcon";
@@ -10,39 +14,88 @@ import Alert from "../../../components/molecules/Alert/Alert";
 import FormControl from "../../../components/molecules/FormControl/FormControl";
 import PageDescriptor from "../../../components/molecules/PageDescriptor/PageDescriptor";
 import Button from "../../../components/atoms/Button/Button";
+import { authenticate } from "../../../lib/services/auth";
+
+// context
+import { StoreContext } from "../../../store/context/store.context";
 
 export default function Login() {
+  const { auth } = useContext(StoreContext);
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [alert, setAlert] = useState({
+    variant: "",
+    show: false,
+    message: ""
+  });
   const [showPassword, setShowPassword] = useState();
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: ""
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().required("Required"),
+      password: Yup.string().required("Required")
+    }),
+    onSubmit: handleSubmit
+  });
+
+  let timer = null;
+
+  async function handleSubmit(values) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+
+    setIsLoading(true);
+    let response = null;
+
+    try {
+      response = await authenticate(values);
+      auth.setAuthUser(response.user);
+      setAlert({show: true, message: response.message, variant: "success"});
+      setIsLoading(false);
+      
+      timer = setTimeout(() => {
+        setAlert(prevState => ({...prevState, show: false, message: ""}));
+        navigate("/dashboard");
+      }, 2000);
+    }
+    catch(error) {
+      const message = error.message || 'Something went wrong';
+      setAlert({show: true, message, variant: "error"});
+      setIsLoading(false);
+      timer = setTimeout(() => {
+        setAlert(prevState => ({...prevState, show: false, message: ""}));
+      }, 3000);
+    }
+  }
 
   function handleShowPassword() {
     setShowPassword(prevState => !prevState);
   }
 
-  function handleSubmit(event) {
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
-
-    console.log(data);
-  }
-
   return (
     <>
-      <Alert variant="error" show/>
+      <Alert variant="error" {...alert} />
       <PageDescriptor 
         title="Login"
         description="Please enter your login details below to access your account." />
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <FormGroup classname="mb-2">
           <Label title="Email/Staff ID" id="username" />
-          <FormControl type="email" id="username" leftIcon={EmailIcon}
-            placeholder="Your email or staff ID" />
+          <FormControl type="text" id="username" leftIcon={EmailIcon} placeholder="Your email or staff ID"
+            {...formik.getFieldProps('username')} hasError={formik.errors.username && formik.touched.username}
+            errorMessage="Required" />
         </FormGroup>
         <FormGroup classname="mb-2">
           <Label title="Password" id="password" />
-          <FormControl type={showPassword ? "text" : "password"} id="password" leftIcon={LockIcon}
-            placeholder="Type your password here">
-            <PasswordToggler show={showPassword} toggle={handleShowPassword}/>
+          <FormControl type={showPassword ? "text" : "password"} id="password" leftIcon={LockIcon} 
+            placeholder="Type your password here" {...formik.getFieldProps('password')}
+            hasError={formik.errors.password && formik.touched.password} errorMessage="Required">
+            <PasswordToggler show={showPassword} toggle={handleShowPassword} />
           </FormControl>
         </FormGroup>
 
@@ -55,7 +108,9 @@ export default function Login() {
         </div>
 
         <div className="flex flex-column">
-          <Button variant="primary" type="submit">Login</Button>
+          <Button variant="primary" type="submit">
+            {isLoading ? 'Loading...' : 'Login'}
+          </Button>
         </div>
       </form>
     </>
