814fbd097cbd6790304dd6801a80f474137a55cd
Bernard Teye <datssosh@gmail.com>
2024-08-08 05:07:57 +0000

    refactor FormControl to receive validation and feedback for end user

--- src/components/molecules/FormControl/FormControl.jsx
+++ src/components/molecules/FormControl/FormControl.jsx
@@ -1,12 +1,17 @@
 import styles from "./FormControl.module.css";
 
-export default function FormControl({leftIcon, children, ...control}) {
+export default function FormControl(
+  {leftIcon, value, onChange, hasError, errorMessage, children, ...control}
+) {
   let LeftIcon = leftIcon;
   return (
-    <div className={styles.formControl}>
-      <LeftIcon />
-      <input {...control} />
-      {children}
+    <div>
+      <div className={`${styles.formControl} ${hasError && styles.formControlError}`}>
+        <LeftIcon />
+        <input {...control} value={value} onChange={onChange}/>
+        {children}
+      </div>
+      {hasError && <small className={styles.formControlFeedback}>{errorMessage}</small>}  
     </div>
   );
 }
